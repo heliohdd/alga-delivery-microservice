d50a842a8c70a3ddf677f9d6bc302189f784db7b
Helio Dourado <heliohdd@yahoo.com.br>
2025-08-01 20:04:21 -0300

    Logic Control Change Status

--- Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryStatus.java
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryStatus.java
@@ -1,5 +1,30 @@
 package com.algaworks.algadelivery.delivery.tracking.domain.model;
 
+import lombok.AllArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+
+@AllArgsConstructor
 public enum DeliveryStatus {
-    DRAFT, WAITING_FOR_COURIER, IN_TRANSIT, DELIVERED
+    DRAFT,
+    WAITING_FOR_COURIER(DRAFT),
+    IN_TRANSIT(WAITING_FOR_COURIER),
+    DELIVERED(IN_TRANSIT);
+
+
+    private final List<DeliveryStatus> previousStatuses;
+
+    DeliveryStatus(DeliveryStatus... previousStatuses) {
+        this.previousStatuses = Arrays.asList(previousStatuses);
+    }
+
+    public boolean canNotChangeTo(DeliveryStatus newStatus) {
+        DeliveryStatus current = this;
+        return !newStatus.previousStatuses.contains(current);
+    }
+
+    public boolean canChangeTo(DeliveryStatus newStatus) {
+        return !canNotChangeTo(newStatus);
+    }
 }
